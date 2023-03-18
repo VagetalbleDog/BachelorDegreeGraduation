25430d74139f57a11d8cba30e2029ff6678a0321
zhuwenfu <zhuwenfu@kuaishou.com>
2023-03-18 20:05:51 +0800

    feat: 前端拉取swagger

--- Apps/packages/website/src/api/index.ts
+++ Apps/packages/website/src/api/index.ts
@@ -3,8 +3,8 @@
 // API 更新时间：
 // API 唯一标识：
 import * as AppControllerGetHello from './AppControllerGetHello';
-import * as wenzhangmokuai from './wenzhangmokuai';
+import * as Article from './Article';
 export default {
   AppControllerGetHello,
-  wenzhangmokuai,
+  Article,
 };
--- Apps/packages/website/src/api/typings.d.ts
+++ Apps/packages/website/src/api/typings.d.ts
@@ -1,6 +1,13 @@
 declare namespace API {
-  type ArticleControllerSearchByContentParams = {
-    /** 根据id查询文章 */
+  type ArticleControllerFindAllParams = {
+    /** query by category */
+    category?: any;
+    /** query by id */
     id?: any;
+  };
+
+  type ArticleControllerSearchByTitleParams = {
+    /** 文章标题搜索 */
+    title?: any;
   };
 }
